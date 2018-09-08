d04a31878c3e68225a9430b8635253d503db96af
maurelian <maurelian@protonmail.ch>
2018-09-08 17:56:27 +0200

    Add SentMessageLogger contract

---
+++ SentMessageLogger.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.20;
+
+contract SentMessageLogger {
+
+  event SentMessage(
+    uint indexed shard_ID,
+    uint sendGas,
+    address sendFromAddress,
+    address sendToAddress,
+    uint value,
+    bytes data,
+    uint indexed base,
+    uint TTL
+  );
+
+  uint TTL = 30; // some number of blocks 
+
+  function send(uint _shard_ID, uint _sendGas, address _sendToAddress, bytes _data) 
+    external
+    payable
+  {
+    uint base = block.number;
+    uint value = msg.value;
+    address sender = msg.sender;
+
+    emit SentMessage(
+      _shard_ID,
+      _sendGas,
+      msg.sender,
+      _sendToAddress,
+      msg.value,
+      _data,
+      block.number,
+      TTL
+    );
+  }
+
+}

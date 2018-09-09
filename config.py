1c705fae51a5b1727f5adbc18c3ec9d577ae4d7d
Vlad Zamfir <vldzmfr@gmail.com>
2018-09-09 07:17:15 +0200

    fixed integration problem

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
 import random as rand
-from blocks import Block
 
 SHARD_IDS = [0, 1, 2]
 VALIDATOR_NAMES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 VALIDATOR_WEIGHTS = {}
 for v in VALIDATOR_NAMES:
     VALIDATOR_WEIGHTS[v] = rand.uniform(5, 25)
-
-GENESIS_BLOCKS = {}
-for i in SHARD_IDS:
-    GENESIS_BLOCKS[i] = Block(i)
 
 TTL_CONSTANT = 10

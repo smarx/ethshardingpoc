44a7564309c2c31e0943e12a7978dc6313bc67ab
tbeiko <t.beiko23@gmail.com>
2018-09-09 11:53:21 +0200

    Graphs are growing

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import random as rand
 
 SHARD_IDS = [0, 1, 2]
-VALIDATOR_NAMES = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+VALIDATOR_NAMES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 VALIDATOR_WEIGHTS = {}
 for v in VALIDATOR_NAMES:
-    VALIDATOR_WEIGHTS[v] = rand.uniform(5, 25)
+    VALIDATOR_WEIGHTS[v] = 1#rand.uniform(5, 25)
 
 TTL_CONSTANT = 10
--- visualizer.py
+++ visualizer.py
@@ -3,24 +3,27 @@ import networkx as nx
 import numpy as np 
 import matplotlib.pyplot as plt
 import random
-import hashlib 
+import hashlib
+from simulator import watcher
+from blocks import Block
+from simulator import GENESIS_BLOCKS
 
 SHARD_COUNT = 3
-MAX_BLOCK_HEIGHT = 5 
+MAX_BLOCK_HEIGHT = 50
 VALIDATOR_COUNT = 3
 SHARD_SPACING_CONSTANT = 3
-VALIDATOR_SPACING_CONSTANT = 0.5 
+VALIDATOR_SPACING_CONSTANT = 1
 blocksPerHeight = {}
 messageMap = {}
 MessageGraph = nx.DiGraph();
-BlocksGraph = nx.Graph();
+BlocksGraph2 = nx.Graph();
 ValidatorLineGraph = nx.Graph();
 previousGoodBlock = [None, None, None]
 
 def checkIfGood(block):
-	return True
+	return block['Winning']
 
-with open("placeholder_blocks.json") as json_file:
+with open("blocks.json") as json_file:
     json_data = json.load(json_file)
 
 blocks = json_data['blocks']
@@ -51,15 +54,33 @@ while i < SHARD_COUNT:
 	i += 1
 	j=0
 
+
+blocks = watcher.get_blocks_from_consensus_messages()
+
+for message in watcher.consensus_messages:
+	block = message.estimate
+	BlocksGraph2.add_node(block)
+	blockPos[block] = (block.height, SHARD_SPACING_CONSTANT*block.shard_ID + message.sender)
+	if block.prevblock is not None and block.prevblock in blocks:
+		BlocksGraph2.add_edge(block, block.prevblock)
+	nx.draw_networkx_nodes(BlocksGraph2, blockPos, node_shape='o', node_color='b', node_size=10)
+	nx.draw_networkx_edges(BlocksGraph2, blockPos) 
+	# nx.draw(BlocksGraph2)
+'''
+
 for height in blocksPerHeight:
 	for block in blocksPerHeight[height]: 
 		blockID = hashlib.sha256(json.dumps(block)).hexdigest()
 		BlocksGraph.add_node(blockID)
+		if block['height'] != 1:
+			BlocksGraph2.add_edge(block['shard'], blockID)
+
 
 		if checkIfGood(block): 
 			if block['height'] != 1: 
 				BlocksGraph.add_edge(previousGoodBlock[block['shard']], blockID)
 			previousGoodBlock[block['shard']] = blockID;
+
 
 		if block['height'] != 1: 
 			for message in block['ReceivedMessages']:
@@ -71,10 +92,10 @@ for height in blocksPerHeight:
 			hashedMessage = hashlib.sha256(json.dumps(message)).hexdigest()
 			messageMap[hashedMessage] = blockID
 			MessageGraph.add_node(hashedMessage)
-			messPos[hashedMessage] = (block['height'] + random.uniform(-0.15, 0.15), SHARD_SPACING_CONSTANT*block['shard'] + VALIDATOR_SPACING_CONSTANT*block['validator'] + random.uniform(-0.15, 0.15))
+			messPos[hashedMessage] = (block['height'] + random.uniform(-0.15, 0.15), SHARD_SPACING_CONSTANT*block['shard'] + VALIDATOR_SPACING_CONSTANT*block['validator']-1 + random.uniform(-0.15, 0.15))
 
 		# Positioning the different shards 
-		blockPos[blockID] = (block['height'], SHARD_SPACING_CONSTANT*block['shard'] + VALIDATOR_SPACING_CONSTANT*block['validator'])
+		blockPos[blockID] = (block['height'], SHARD_SPACING_CONSTANT*block['shard'] + VALIDATOR_SPACING_CONSTANT*block['validator']-1)
 		blockLabels[blockID] = "Shard: " + str(block['shard']) + " Height: " + str(block['height'])
 
 		nonMessageNodesinMessageGraph = []
@@ -89,15 +110,16 @@ for height in blocksPerHeight:
 				messPos[node] = blockPos[node]
 
 		nx.draw_networkx_edges(ValidatorLineGraph, validatorLinePos, style='dashed')
-		nx.draw_networkx_edges(BlocksGraph, blockPos, edge_color='g', width = 3)
+		nx.draw_networkx_edges(BlocksGraph, blockPos, edge_color='g', width = 2)
+		nx.draw_networkx_edges(BlocksGraph2, blockPos, edge_color='y', width = 1)
 		nx.draw_networkx_nodes(MessageGraph, messPos, node_shape='o', node_color='b', node_size=10)
 		nx.draw_networkx_edges(MessageGraph, messPos) 
 		nx.draw_networkx_nodes(BlocksGraph, blockPos, node_shape='s', node_size=100)
 		# Shows the block height and shard on each block 
 		# nx.draw_networkx_labels(BlocksGraph, blockPos, blockLabels)
-
-		plt.axis('off')
-		plt.draw()
-		plt.pause(0.001)
+'''
+plt.axis('off')
+plt.draw()
+plt.pause(0.001)
 
 plt.show()

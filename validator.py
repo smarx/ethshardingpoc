4fa4b517532c5128e0cf312b064d8e95ab5c2c25
Vlad Zamfir <vldzmfr@gmail.com>
2018-09-09 07:15:41 +0200

    bug/integration fixes

--- validator.py
+++ validator.py
@@ -1,12 +1,11 @@
 from blocks import Block
+from blocks import ReceivedLog
 from config import SHARD_IDS
 from config import VALIDATOR_NAMES
 from config import VALIDATOR_WEIGHTS
-from config import GENESIS_BLOCKS
 from config import TTL_CONSTANT
 
 from fork_choice import sharded_fork_choice
-
 
 class ConsensusMessage:
     def __init__(self, block, name, justification):
@@ -16,20 +15,31 @@ class ConsensusMessage:
 
         assert isinstance(self.estimate, Block), "expected block"
         assert self.sender in VALIDATOR_NAMES
+
+        if len(self.justification) == 0:
+            self.height = 0
+
         for m in self.justification:
             assert isinstance(m, ConsensusMessage), "expected justification to contain consensus messages"
-
             self.height = 0
-            if m.sender = self.sender:
+            if m.sender == self.sender:
                 self.height += 1
 
 class Validator:
-    def __init__(self, name):
+    def __init__(self, name, starting_blocks):
         assert name in VALIDATOR_NAMES, "expected a validator name"
         self.name = name
         self.consensus_messages = []
+        assert isinstance(starting_blocks, dict), "expected dict"
+        for ID in SHARD_IDS:
+            assert ID in starting_blocks.keys(), "expected to have starting blocks for all shard IDs"
+        for b in starting_blocks.values():
+            assert isinstance(b, Block), "expected starting blocks to be blocks"
 
-    def receive_protocol_messages(messages):
+        self.starting_blocks = starting_blocks
+
+
+    def receive_consensus_messages(self, messages):
         for m in messages:
             self.consensus_messages.append(m)
 
@@ -49,7 +59,7 @@ class Validator:
 
     def get_weighted_blocks(self):
         weighted_blocks = {}
-        L_M = latest_messages(self.consensus_messages)
+        L_M = self.latest_messages()
         for v in VALIDATOR_NAMES:
             if L_M[v] is not None:
                 if L_M[v].estimate in weighted_blocks.keys():
@@ -68,10 +78,13 @@ class Validator:
     def fork_choice(self):
         # the blocks in the view are the genesis blocks and blocks from consensus messages
         blocks = self.get_blocks_from_consensus_messages()
-        for b in GENESIS_BLOCKS:
+        for b in self.starting_blocks.values():
             blocks.append(b)
 
-        return sharded_fork_choice(GENESIS_BLOCKS, blocks, self.get_weighted_blocks())
+        for b in blocks:
+            print "b", b
+
+        return sharded_fork_choice(self.starting_blocks, blocks, self.get_weighted_blocks())
 
 
     def make_block(self, shard_ID, data, TTL=TTL_CONSTANT):
@@ -105,6 +118,8 @@ class Validator:
         # which have the following newly received payloads:
         newly_received_payloads = {}
         for ID in SHARD_IDS:
+            if ID == shard_ID:
+                continue
             newly_received_payloads[ID] = []
             for m in newly_received_messages[ID]:
                 newly_received_payloads[ID].append(m.message_payload)
@@ -114,21 +129,29 @@ class Validator:
         '''
 
         # this is where we have this function that produces the new vm state and the new outgoing payloads
-        new_vm_state, new_outgoing_payloads = INTEGRATE_HERE(prevblock.vm_state, data, newly_received_payloads)
+        # new_vm_state, new_outgoing_payloads = INTEGRATE_HERE(prevblock.vm_state, data, newly_received_payloads)
         # need new_outgoing_payloads is a dict of shard id to new payloads
 
-        # we now package the sent_log with new messages that deliver these payloads
+        new_vm_state = prevblock.vm_state
+        new_outgoing_payloads = {}
         for ID in SHARD_IDS:
-            new_sent_messages = []
+            new_outgoing_payloads[ID] = []
+
+        # we now package the sent_log with new messages that deliver these payloads
+        new_sent_messages = []
+        shard_IDs = []
+        for ID in SHARD_IDS:
             for payload in new_outgoing_payloads[ID]:
                 new_sent_messages.append(Message(fork_choice[ID], TTL, payload))
-                sent_log = prevblock.sent_log.add_sent_messages(new_sent_messages)
+                shard_IDs.append(ID)
+        
+        sent_log = prevblock.sent_log.add_sent_messages(shard_IDs, new_sent_messages)
+
 
         return Block(shard_ID, prevblock, data, sent_log, received_log, new_vm_state)
 
-    def make_new_consensus_message(self, shard_ID, data, TTL=TTL_CONSTANT)
-
-        new_block = make_block(self, shard_ID, data, TTL)
+    def make_new_consensus_message(self, shard_ID, data, TTL=TTL_CONSTANT):
+        new_block = self.make_block(shard_ID, data, TTL)
         new_message = ConsensusMessage(new_block, self.name, self.consensus_messages)
         self.consensus_messages.append(new_message)
         return new_message
